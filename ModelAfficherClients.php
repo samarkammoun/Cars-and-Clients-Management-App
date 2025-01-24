6b7bab34da8bf5040da3b6ab2701b22ad3a534ee
Samar Kammoun <samar.kammoun2003@gmail.com>
2025-01-24 10:41:52 +0100

    Update ModelAfficherClients.php

--- ModelAfficherClients.php
+++ ModelAfficherClients.php
@@ -89,7 +89,7 @@
         <div class="table-container">
             <h2 class="mb-4">Liste des Clients</h2>
             <?php
-            require "ToConnectTp4.php";
+            require "ToConnect.php";
 
             $sql = "SELECT * FROM client";
             $client = $pdo->query($sql);
