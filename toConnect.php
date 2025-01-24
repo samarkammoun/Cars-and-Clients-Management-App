83a20c8bb377c07efd33380486a122d7cf7e9ef4
Samar Kammoun <samar.kammoun2003@gmail.com>
2025-01-24 11:08:19 +0100

    Update toConnect.php

--- toConnect.php
+++ toConnect.php
@@ -1,9 +1,33 @@
 <?php
 
-// Charger le fichier .env 
-require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+/**
+ * Load environment variables from a .env file.
+ *
+ * @param string $path Path to the .env file.
+ */
+function loadEnv($path) {
+    // Check if the .env file exists
+    if (!file_exists($path)) {
+        throw new Exception('.env file not found');
+    }
+
+    // Read the .env file line by line
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+
+        $value = trim($value, " \t\n\r\0\x0B\"'");
+
+        $_ENV[$name] = $value;
+    }
+}
+
+
+loadEnv(__DIR__ . '/.env');
 
 // Definition des constantes en utilisant les variables d'environnement
 define('SERVER', $_ENV['DB_HOST']);
@@ -11,7 +35,6 @@ define('DBNAME', $_ENV['DB_NAME']);
 define('USER', $_ENV['DB_USER']);
 define('PASSWD', $_ENV['DB_PASSWORD']);
 
-// Création du dns
 $dns = "mysql:host=" . SERVER . ";dbname=" . DBNAME;
 
 try {
